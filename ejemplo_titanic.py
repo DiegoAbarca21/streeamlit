6603da9ac810a397c41354a56fecdcc16695306e
DiegoAbarca21 <diegoabva2003@gmail.com>
2024-10-22 15:38:08 -0300

    Update ejemplo_titanic.py

--- ejemplo_titanic.py
+++ ejemplo_titanic.py
@@ -19,13 +19,15 @@ with st.sidebar:
     # Crea un control deslizante (slider) que permite al usuario seleccionar un número de bins
     # en el rango de 0 a 10, con un valor predeterminado de 2.
     div = st.slider('Número de bins:', 0, 10, 2)
+    colorslide = st.slider('color:', "blue", "green", "red")
     
     # Muestra el valor actual del slider en la barra lateral.
     st.write("Bins=", div)
+    st.write("color=", colorslide)
 
 # Desplegamos un histograma con los datos del eje X
 fig, ax = plt.subplots(1, 2, figsize=(10, 3))
-ax[0].hist(df["Age"], bins=div)
+ax[0].hist(df["Age"], bins=div, color=colorslide)
 ax[0].set_xlabel("Edad")
 ax[0].set_ylabel("Frecuencia")
 ax[0].set_title("Histograma de edades")
